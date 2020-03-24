fddb08c38c5e3fdd0f0d63c8bc5f281875f774fd
jvergara-runbuggy <60939600+jvergara-runbuggy@users.noreply.github.com>
2020-03-24 18:57:27 -0400

    update main go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Jesus is the Mesiah"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
